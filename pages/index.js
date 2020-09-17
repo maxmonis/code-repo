b3994ecf65ece8afb1475bbb7db15e0f0894e01d
Max Monis <mmonis77@gmail.com>
2020-09-17 09:39:09 -0400

    allow access of challenges in index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 const Index = () => {
+  const [challenges, setChallenges] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+  useEffect(() => {
+    const getChallenges = () => {
+      firebase.db
+        .collection('challenges')
+        .orderBy('created', 'desc')
+        .onSnapshot(handleSnapshot);
+    };
+    getChallenges();
+  }, []);
+  function handleSnapshot(snapshot) {
+    const challenges = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setChallenges(challenges);
+  }
   return (
-    <div className='container'>
+    <div>
       <Layout>
         <div className='container'>
-          <h1>Index</h1>
+          {challenges.map((challenge) => (
+            <p>{JSON.stringify(challenge)}</p>
+          ))}
         </div>
       </Layout>
     </div>
