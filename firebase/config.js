019b3c29daf3a2b69f634b40803edbefffe40326
Max Monis <mmonis77@gmail.com>
2020-09-21 08:32:17 -0400

    config of env vars

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+export default config;
---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DB_URL: process.env.DB_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    SENDER_ID: process.env.SENDER_ID,
+    APP_ID: process.env.APP_ID,
+  },
+};
